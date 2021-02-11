650086ef797a2ad19c1e9f23b7455ddad8784bf0
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2021-02-11 21:35:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Installation
+
+1. `pip install gym`
+2. `cmake CMakeLists.txt`
+3. `pip install 'gym[atari]'`
---
+++ main.py
@@ -0,0 +1,11 @@
+import gym
+
+if __name__ == '__main__':
+    env = gym.make('SpaceInvaders-v0')
+    env.reset()
+
+    for _ in range(1000):
+        env.render()
+        env.step(env.action_space.sample())  # take a random action
+
+    env.close()
